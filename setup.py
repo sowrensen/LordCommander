abedc1139f2dc1cc34085b80ca2d4110686d94ae
Sowren Sen <sowrensen@gmail.com>
2020-10-04 18:48:30 +0600

    Fixed some classifiers

--- setup.py
+++ setup.py
@@ -38,14 +38,13 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: Unix",
-        "Operating System :: POSIX :: Linux"
-        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Topic :: Software Development :: Version Control"
+        "Topic :: Software Development :: Version Control",
         "Topic :: System :: Shells",
         "Topic :: System :: Installation/Setup",
-        "Topic :: Terminals",
+        "Topic :: Terminals"
     ]
 )
