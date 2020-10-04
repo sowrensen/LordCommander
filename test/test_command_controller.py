d4f3b9e36e87580be456499be2d2307f7a04e7b6
Sowren Sen <sowrensen@gmail.com>
2020-10-04 16:47:09 +0600

    Added a new test for directory controller

---
+++ test/test_command_controller.py
@@ -0,0 +1,5 @@
+from colr import color
+
+from lordcommander.controllers import DirectoryController
+from lordcommander.output import Output
+from .commons import *
--- test/test_directory_controller.py
+++ test/test_directory_controller.py
@@ -15,10 +15,20 @@ def test_adding_an_instance(capsys):
     assert 'pro1ins1' in instances
 
 
-def test_add_without_name(capsys):
+def test_adding_an_instance_without_name(capsys):
     generate_dummy_data_with_no_instance()
     testdb = get_test_shelve_file()
     dc = DirectoryController(testdb['projects']['project1']['instances'])
     dc.add()
     captured = capsys.readouterr()
     assert captured.out == color("Aborting! No argument has been provided.", Output.DANGER) + "\n"
+
+
+def test_removing_an_instance():
+    generate_full_dummy_data()
+    testdb = get_test_shelve_file()
+    dc = DirectoryController(testdb['projects']['project1']['instances'])
+    dc.clear('pro1ins1')
+    instances = testdb['projects']['project1']['instances']
+    close_db(testdb)
+    assert 'pro1ins1' not in instances
